028dcc666fadc094057c0056c090dfcbdb5cb5f4
MayThuHtun <maythutun.ucsmdy@gmail.com>
2018-06-06 13:15:29 -0700

    Commit ex11

--- ex11.py
+++ ex11.py
@@ -1,7 +1,7 @@
 print("How old are you?"),
-age = raw_input()
+age = raw_input('Enter Your Age : ')
 print("How tall are you?"),
-height = raw_input()
+height = raw_input('Enter Your Height : ')
 print("How much do you weight?"),
-weight = raw_input()
+weight = raw_input('Enter Your Weight " ')
 print("So, you're %r old, %r tall and %r heavy." %(age, height, weight))
