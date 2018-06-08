ae396841456e6890ba2668015e6c7d3c7557b499
MayThuHtun <maythutun.ucsmdy@gmail.com>
2018-06-08 15:28:09 -0700

    Commit ex23

--- ex23.py
+++ ex23.py
@@ -1,5 +1,7 @@
 import sys
-script, encoding, error = sys.argv
+script = sys.argv
+encoding = sys.argv
+error = sys.argv
 
 def main(language_file, encoding, errors):
     line = language_file.readline()
@@ -12,9 +14,6 @@ def print_line (line, encoding, errors):
     next_lang = line.strip()
     raw_bytes = next_lang.encode(encoding, errors=errors)
     cooked_string = raw_bytes.decode(encoding, errors=errors)
-
     print(raw_bytes, "<===>", cooked_string)
-
-languages = open("languages.txt", encoding="utf-8")
-
-main(languages, encoding, error)
+    languages = open("languages.txt", encoding="utf-8")
+    main(languages, encoding, error)
