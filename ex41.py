d251ad68dab040967b60d270ce2c6652b817631a
MayThuHtun <maythutun.ucsmdy@gmail.com>
2018-06-18 22:13:28 -0700

    Commit ex44

--- ex41.py
+++ ex41.py
@@ -4,33 +4,34 @@ import sys
 WORD_URL = "http://learncodethehardway.org/words.txt"
 WORDS = []
 
-PHRASES = {
-        "class %%%(%%%):":
-         "Make a class named %%% that is-a %%%.",
-        "class %%%(object):\n\tdef __init__(self, ***)" :
-         "class %%% has-a __init__ that takes self and *** params.",
-        "class %%%(object):\n\tdef ***(self, @@@)":
-         "class %%% has-a function *** that takes self and @@@ params.",
-        "*** = %%%()":
-         "Set *** to an instance of class %%%.",
-        "***.***(@@@)":
-         "From *** get the *** function, call it with params self, @@@.",
-        "***.*** = '***'":
-         "From *** get the *** attribute and set it to '***'."
-    }
+PHRASES = [
+        "class %%%(%%%):"
+            "Make a class named %%% that is-a %%%."
+        "class %%%(object):\n\tdef __init__(self, ***)" 
+            "class %%% has-a __init__ that takes self and *** params."
+        "class %%%(object):\n\tdef ***(self, @@@)"
+            "class %%% has-a function *** that takes self and @@@ params.",
+        "*** = %%%()"
+            "Set *** to an instance of class %%%."
+        "***.***(@@@)"
+            "From *** get the *** function, call it with params self, @@@."
+        "***.*** = '***'"
+            "From *** get the *** attribute and set it to '***'."
+    ]
 
 # do they want to drill phrases first
-PHRASE_FIRST = False
+
 if len(sys.argv) == 2 and sys.argv[1] == "english":
     PHRASE_FIRST = True
+else:
+    PHRASE_FIRST = False
     
 #load up the words from the website
 for word in urlopen(WORD_URL).readlines():
-    WORDS.append(word.strip())
+    WORDS.append(str(word.strip(),encoding="utf_8"))
 
 def convert(snippet, phrase):
-    class_names = [w.capitalize() for w in
-            random.sample(WORDS, snippet.count("%%%"))]
+    class_names = [w.capitalize() for w in random.sample(WORDS, snippet.count("%%%"))]
     other_names = random.sample(WORDS, snippet.count("***"))
     results = []
     param_names = []
